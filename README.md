2597487372d12a54cf67b52119e5d584f554b77f
Nazar <Kapushchaknazar@gmail.com>
2023-09-02 19:47:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Spotify Clone App
+
+This is a Spotify clone app built with React that utilizes the Rapid API to provide users with access to the most popular tracks and artists. The app focuses on a specific country and genre of music to deliver a tailored music experience.
+
+## Features
+- 🎵 **Music Discovery**: Discover the most popular tracks and artists in your chosen country and genre of music.
+- 🔄 **Real-Time Data**: The app uses the Rapid API to fetch real-time data, ensuring that you stay updated with the latest trends.
+- 📱 **Responsive Design**: Enjoy a seamless experience on both desktop and mobile devices.
+- 🔐 **User Authentication**: Log in with your Spotify account to access additional features (coming soon).
+
+## Technologies Used
+- ⚛️ **React\Next.js**: The app is built using React, a popular JavaScript library for building user interfaces.
+- 🚀 **Rapid API**: Rapid API is used to access music data and enhance the user experience.
+- 🌐 **HTML & CSS**: The app's layout and styling are done using HTML and CSS.
+- 🔥 **Firebase (Coming Soon)**: User authentication and additional features will be implemented using Firebase.
+
+
+Screenshot of local version:
+![image info](src/assets/Screenshot.png)
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam.p.rapidapi.com',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '80ef04bd8fmsh57c7be7e05c748ap191c99jsn4dae6214d1a4');
+      headers.set('X-RapidAPI-Host', 'shazam.p.rapidapi.com');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/track' }),
+    getSongDetails: builder.query({ query: ({ songid }) => `songs/get-details?key=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/songs/get-related-artist?id=${songid}` }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
